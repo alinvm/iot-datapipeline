3cadbe5e11f898b6ba5949fc5b867f7e1b57321f
alinvm <alinvm@gmail.com>
2025-11-08 20:49:01 +0100

    Ingress Application starter

---
+++ ingress/src/main/java/org/example/iot/ingress/IngressApplication.java
@@ -0,0 +1,11 @@
+package org.example.iot.ingress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngressApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(IngressApplication.class, args);
+    }
+}
